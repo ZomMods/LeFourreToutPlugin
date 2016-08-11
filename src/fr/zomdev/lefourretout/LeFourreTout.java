817f9043154ff95cb7be4072594d44ebf0b50cea
ZomDev <zomreview@gmail.com>
2016-08-11 17:47:40 +0200

    First Commit

---
+++ src/fr/zomdev/lefourretout/LeFourreTout.java
@@ -0,0 +1,85 @@
+package fr.zomdev.lefourretout;
+
+import fr.zomdev.lefourretout.cmds.LFTCommand;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by ZomDev on 09/08/2016.
+ */
+public class LeFourreTout extends JavaPlugin implements Listener {
+
+    public static LeFourreTout instance;
+
+    public static LeFourreTout getInstance() {
+        return instance;
+    }
+
+    PluginManager pm = Bukkit.getPluginManager();
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        getCommand("lft").setExecutor(new LFTCommand(this));
+        getCommand("lft2").setExecutor(this);
+        pm.registerEvents(this, this);
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        Player p = (Player) sender;
+        if (p.isOp()) {
+            Bukkit.broadcastMessage(ChatColor.BLUE + "Bienvenue sur Le Fourre-Tout");
+        } else {
+            p.sendMessage(ChatColor.DARK_RED + "Vous n'avez pas la permission d'exécuter cette commande !");
+        }
+
+        return false;
+    }
+
+    @EventHandler
+    public void onGrassBreak(BlockBreakEvent e) {
+        Block b = e.getBlock();
+        Player p = e.getPlayer();
+        if (b.getType() == Material.GRASS) {
+            if (!p.isSneaking()) {
+                p.sendMessage(ChatColor.DARK_RED + "Vous devez être en sneak pour activer l'event !");
+            } else {
+                p.sendMessage(ChatColor.GREEN + "Vous avez cassé un block de " + ChatColor.GOLD + b.getType().name());
+            }
+        }
+    }
+
+    @EventHandler
+    public void onLavaPlace(BlockPlaceEvent e) {
+        Block b = e.getBlockPlaced();
+        Player p = e.getPlayer();
+        if (b.getType() == Material.DIRT) {
+            if (!p.hasPermission("lft.lava.place")) {
+                e.setCancelled(true);
+                p.sendMessage(ChatColor.RED + " Vous n'avez pas la permission de poser de la lave !");
+            } else {
+                e.setCancelled(false);
+            }
+        }
+    }
+
+}
---
+++ src/fr/zomdev/lefourretout/cmds/LFTCommand.java
@@ -0,0 +1,45 @@
+package fr.zomdev.lefourretout.cmds;
+
+import fr.zomdev.lefourretout.LeFourreTout;
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+
+/**
+ * Created by Thomas on 09/08/2016.
+ */
+public class LFTCommand implements CommandExecutor {
+
+    private final LeFourreTout pl;
+    private final FileConfiguration config;
+
+    public LFTCommand(LeFourreTout pl) {
+        this.pl = pl;
+        this.config = pl.getConfig();
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        Player p = (Player) sender;
+        if (args.length == 0) {
+            p.teleport(new Location(Bukkit.getWorld(config.getString("Spawn.World")), config.getDouble("Spawn.X"), config.getDouble("Spawn.Y"), config.getDouble("Spawn.Z"), (float)config.get("Spawn.Yaw"),(float)config.get("Spawn.Pitch")));
+        }else if(args.length == 1){
+            if(args[0].equalsIgnoreCase("set")){
+                config.set("Spawn.X", p.getLocation().getX());
+                config.set("Spawn.Y", p.getLocation().getY());
+                config.set("Spawn.Z", p.getLocation().getZ());
+                config.set("Spawn.Yaw", p.getLocation().getYaw());
+                config.set("Spawn.Pitch", p.getLocation().getPitch());
+                config.set("Spawn.World", p.getWorld().getName());
+                pl.saveConfig();
+                p.sendMessage(ChatColor.GREEN + "Vous avez changé la location du lft !");
+            }
+        }
+        return false;
+    }
+}
